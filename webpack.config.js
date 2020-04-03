cfe3c154a529cbe08815efb08a4b7cb53c30494a
Hadyan Ahmad <hadyandev@gmail.com>
2020-04-04 00:26:22 +0700

    07. Using Plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -36,4 +37,12 @@ module.exports = {
       },
     ],
   },
+  /* plugin */
+  plugins: [
+    /* HTML Webpack Plugin */
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: "index.html",
+    }),
+  ],
 };
